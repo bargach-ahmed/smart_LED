ad96afa0d7888b4f9214fbb8df8f9d33fe832b00
bargach-ahmed <73179171+bargach-ahmed@users.noreply.github.com>
2022-01-09 20:50:42 +0100

    Add files via upload

---
+++ led_control_with_bluetooth.ino
@@ -0,0 +1,20 @@
+void setup() {
+Serial.begin(9600);
+ pinMode(8, OUTPUT); // put your setup code here, to run once:
+ }
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+ if(Serial.available()>0)
+   {     
+      char data= Serial.read(); // reading the data received from the bluetooth module
+      switch(data)
+      {
+        case 'a': digitalWrite(8, HIGH);break; // when a is pressed on the app on your smart phone
+        case 'd': digitalWrite(8, LOW);break; // when d is pressed on the app on your smart phone
+        default : break;
+      }
+      Serial.println(data);
+   }
+   delay(50);
+}
